695e475cc86f99a2e03e7e2769d54685240c9f50
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-07-04 19:57:47 -0400

    Add ISnowflake

---
+++ DSharpPlus.Abstractions/Entities/ISnowflake.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DSharpPlus.Abstractions.Entities.Clients;
+
+namespace DSharpPlus.Abstractions.Entities
+{
+	/// <summary>
+	/// Represents a unique Discord object.
+	/// </summary>
+	public interface ISnowflakeObject
+	{
+		/// <summary>
+		/// The Id of this snowflake.
+		/// </summary>
+		public ulong Id { get; }
+		
+		/// <summary>
+		/// When this snowflake was created.
+		/// </summary>
+		public DateTimeOffset CreationDate => DiscordEpoch.AddMilliseconds(Id >> 22);
+
+		internal IDiscordClient Client { get; set; }
+		private static DateTimeOffset DiscordEpoch = new DateTimeOffset(2015, 1, 1, 0, 0, 0, TimeSpan.Zero);
+		
+	}
+}
