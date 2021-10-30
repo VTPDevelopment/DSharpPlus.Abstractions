1b880659100d3069a45ec69b8cb8b2f72212336e
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-10-30 04:05:12 -0400

    Remove IDictionary inheritence

--- DSharpPlus.Abstractions/LazyUpdateDictionary.cs
+++ DSharpPlus.Abstractions/LazyUpdateDictionary.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace DSharpPlus.Abstractions
 {
-	public sealed class LazyUpdateDictionary<TKey, TValue, TUnderlying> : IDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, TValue>
+	public sealed class LazyUpdateDictionary<TKey, TValue, TUnderlying> : IReadOnlyDictionary<TKey, TValue>
 	{
 		private readonly IReadOnlyDictionary<TKey, TUnderlying> _underlyingDictionary;
 		private readonly Dictionary<TKey, TValue> _backingDictionary = new();
@@ -22,19 +22,9 @@ namespace DSharpPlus.Abstractions
 			foreach (var (k, v) in _underlyingDictionary)
 				yield return new(k, _cast(v));
 		}
-		IEnumerator IEnumerable.GetEnumerator() =>  GetEnumerator();
-		public void Add(KeyValuePair<TKey, TValue> item) => throw new InvalidOperationException();
-		public void Clear() => throw new InvalidOperationException();
-		public bool Contains(KeyValuePair<TKey, TValue> item) => _underlyingDictionary.ContainsKey(item.Key);
-		public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => throw new InvalidOperationException();
-		public bool Remove(KeyValuePair<TKey, TValue> item) => throw new InvalidOperationException();
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-		int ICollection<KeyValuePair<TKey, TValue>>.Count => _underlyingDictionary.Count;
-
-		public bool IsReadOnly => true;
-		public void Add(TKey key, TValue value) => throw new InvalidOperationException();
-		bool IDictionary<TKey, TValue>.ContainsKey(TKey key) => _underlyingDictionary.ContainsKey(key);
-		bool IReadOnlyDictionary<TKey, TValue>.TryGetValue(TKey key, out TValue value)
+		public bool TryGetValue(TKey key, out TValue value)
 		{
 			value = default;
 
@@ -49,25 +39,17 @@ namespace DSharpPlus.Abstractions
 
 			return true;
 		}
-		public bool Remove(TKey key) => throw new InvalidOperationException();
-		bool IReadOnlyDictionary<TKey, TValue>.ContainsKey(TKey key) => _underlyingDictionary.ContainsKey(key);
-		bool IDictionary<TKey, TValue>.TryGetValue(TKey key, out TValue value) 
-			=> (this as IReadOnlyDictionary<TKey, TValue>).TryGetValue(key, out value);
+		public bool ContainsKey(TKey key) => _underlyingDictionary.ContainsKey(key);
 
 		public TValue this[TKey key]
 		{
-			get => (this as IDictionary<TKey, TValue>).TryGetValue(key, out var value) ? value : throw new KeyNotFoundException();
+			get => TryGetValue(key, out var value) ? value : throw new KeyNotFoundException();
 			set => throw new InvalidOperationException();
 		}
 
-		IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys => this.Select(kvp => kvp.Key);
+		public IEnumerable<TKey> Keys => this.Select(kvp => kvp.Key);
 
-		IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values => this.Select(kvp => kvp.Value);
-
-		ICollection<TKey> IDictionary<TKey, TValue>.Keys => new List<TKey>((this as IReadOnlyDictionary<TKey, TValue>).Keys);
-
-		ICollection<TValue> IDictionary<TKey, TValue>.Values => new List<TValue>((this as IReadOnlyDictionary<TKey, TValue>).Values);
-
-		int IReadOnlyCollection<KeyValuePair<TKey, TValue>>.Count => _underlyingDictionary.Count;
+		public IEnumerable<TValue> Values => this.Select(kvp => kvp.Value);
+		public int Count => _underlyingDictionary.Count;
 	}
 }
