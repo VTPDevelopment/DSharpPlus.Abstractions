b1f171b4d6c74d5b244929fe0b6d7e6547a45086
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-10-30 04:01:11 -0400

    Rewrite LazyUpdateDictionary.cs

--- DSharpPlus.Abstractions/LazyUpdateDictionary.cs
+++ DSharpPlus.Abstractions/LazyUpdateDictionary.cs
@@ -1,79 +1,73 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DSharpPlus.Abstractions
 {
 	public sealed class LazyUpdateDictionary<TKey, TValue, TUnderlying> : IDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, TValue>
 	{
-		private int _count;
-		private ICollection<TKey> _keys;
-		private ICollection<TValue> _values;
-		private int _count1;
-		private IEnumerable<TKey> _keys1;
-		private IEnumerable<TValue> _values1;
-
+		private readonly IReadOnlyDictionary<TKey, TUnderlying> _underlyingDictionary;
+		private readonly Dictionary<TKey, TValue> _backingDictionary = new();
+		private readonly Func<TUnderlying, TValue> _cast;
+		
+		public LazyUpdateDictionary(IReadOnlyDictionary<TKey, TUnderlying> underlyingDictionary, Func<TUnderlying, TValue> castFunc)
+		{
+			_cast = castFunc;
+			_underlyingDictionary = underlyingDictionary;
+		}
+		
 		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
 		{
-			yield break;
+			foreach (var (k, v) in _underlyingDictionary)
+				yield return new(k, _cast(v));
 		}
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
-		}
-		public void Add(KeyValuePair<TKey, TValue> item) { }
-		public void Clear() { }
-		public bool Contains(KeyValuePair<TKey, TValue> item)
-		{
-			return false;
-		}
-		public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) { }
-		public bool Remove(KeyValuePair<TKey, TValue> item)
-		{
-			return false;
-		}
+		IEnumerator IEnumerable.GetEnumerator() =>  GetEnumerator();
+		public void Add(KeyValuePair<TKey, TValue> item) => throw new InvalidOperationException();
+		public void Clear() => throw new InvalidOperationException();
+		public bool Contains(KeyValuePair<TKey, TValue> item) => _underlyingDictionary.ContainsKey(item.Key);
+		public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => throw new InvalidOperationException();
+		public bool Remove(KeyValuePair<TKey, TValue> item) => throw new InvalidOperationException();
 
-		int ICollection<KeyValuePair<TKey, TValue>>.Count => _count;
+		int ICollection<KeyValuePair<TKey, TValue>>.Count => _underlyingDictionary.Count;
 
-		public bool IsReadOnly { get; set; }
-		public void Add(TKey key, TValue value) { }
-		bool IDictionary<TKey, TValue>.ContainsKey(TKey key)
-		{
-			return false;
-		}
+		public bool IsReadOnly => true;
+		public void Add(TKey key, TValue value) => throw new InvalidOperationException();
+		bool IDictionary<TKey, TValue>.ContainsKey(TKey key) => _underlyingDictionary.ContainsKey(key);
 		bool IReadOnlyDictionary<TKey, TValue>.TryGetValue(TKey key, out TValue value)
 		{
 			value = default;
-			return false;
+
+			if (!_underlyingDictionary.ContainsKey(key))
+			{
+				_backingDictionary.Remove(key);
+				return false;
+			}
+
+			if (!_backingDictionary.TryGetValue(key, out value))
+				value = _backingDictionary[key] = _cast(_underlyingDictionary[key]);
+
+			return true;
 		}
-		public bool Remove(TKey key)
-		{
-			return false;
-		}
-		bool IReadOnlyDictionary<TKey, TValue>.ContainsKey(TKey key)
-		{
-			return false;
-		}
-		bool IDictionary<TKey, TValue>.TryGetValue(TKey key, out TValue value)
-		{
-			value = default;
-			return false;
-		}
+		public bool Remove(TKey key) => throw new InvalidOperationException();
+		bool IReadOnlyDictionary<TKey, TValue>.ContainsKey(TKey key) => _underlyingDictionary.ContainsKey(key);
+		bool IDictionary<TKey, TValue>.TryGetValue(TKey key, out TValue value) 
+			=> (this as IReadOnlyDictionary<TKey, TValue>).TryGetValue(key, out value);
 
 		public TValue this[TKey key]
 		{
-			get => default;
-			set { }
+			get => (this as IDictionary<TKey, TValue>).TryGetValue(key, out var value) ? value : throw new KeyNotFoundException();
+			set => throw new InvalidOperationException();
 		}
 
-		IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys => _keys1;
+		IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys => this.Select(kvp => kvp.Key);
 
-		IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values => _values1;
+		IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values => this.Select(kvp => kvp.Value);
 
-		ICollection<TKey> IDictionary<TKey, TValue>.Keys => _keys;
+		ICollection<TKey> IDictionary<TKey, TValue>.Keys => new List<TKey>((this as IReadOnlyDictionary<TKey, TValue>).Keys);
 
-		ICollection<TValue> IDictionary<TKey, TValue>.Values => _values;
+		ICollection<TValue> IDictionary<TKey, TValue>.Values => new List<TValue>((this as IReadOnlyDictionary<TKey, TValue>).Values);
 
-		int IReadOnlyCollection<KeyValuePair<TKey, TValue>>.Count => _count1;
+		int IReadOnlyCollection<KeyValuePair<TKey, TValue>>.Count => _underlyingDictionary.Count;
 	}
 }
