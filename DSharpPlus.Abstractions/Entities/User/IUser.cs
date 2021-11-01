c95a7acc4e4d37c670e65b6781fa17cbf503810c
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-10-31 23:13:56 -0400

    Add IsBot & GetAvatarUrl to IUser, and implemnent the interface

--- DSharpPlus.Abstractions/Entities/User/IUser.cs
+++ DSharpPlus.Abstractions/Entities/User/IUser.cs
@@ -7,18 +7,21 @@ namespace DSharpPlus.Abstractions.Entities
 	{
 		public string Username { get; }
 		
-		public DiscordColor BannerColor { get; }
+		public DiscordColor? BannerColor { get; }
 		
 		public string? BannerUrl { get; }
 
 		public string AvatarUrl { get; }
 
-		public UserFlags Flags { get; }
+		public UserFlags? Flags { get; }
 
 		public string Mention { get; }
 
 		public bool IsCurrent { get; }
-		
 
+		public bool IsBot { get; }
+
+		public string GetAvatarUrl(ImageFormat fmt, ushort size = 1024);
+		
 	}
 }
---
+++ DSharpPlus.Abstractions/Entities/User/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.Abstractions.Entities
+{
+	public sealed class User : IUser
+	{
+		private readonly DiscordUser _user;
+
+		internal User(DiscordUser user) => _user = user;
+
+		public ulong Id => _user.Id;
+		public DateTimeOffset CreationTimestamp => _user.CreationTimestamp;
+		public string Username => _user.Username;
+		public DiscordColor? BannerColor => throw new NotSupportedException("D#+ bad"); //TODO: OPEN A PR FOR THIS!!! THIS SHOULD BE ON THE USER OBJECT NOT THE MEMBER
+		public string? BannerUrl => throw new NotSupportedException("Banner url is set on the member. This is an issue with D#+.");
+		public string AvatarUrl => _user.AvatarUrl;
+		public UserFlags? Flags => _user.Flags;
+		public string Mention => _user.Mention;
+		public bool IsCurrent => _user.IsCurrent;
+		public bool IsBot => _user.IsBot;
+
+		public string GetAvatarUrl(ImageFormat fmt, ushort size = 1024)
+			=> _user.GetAvatarUrl(fmt, size);
+	}
+}
