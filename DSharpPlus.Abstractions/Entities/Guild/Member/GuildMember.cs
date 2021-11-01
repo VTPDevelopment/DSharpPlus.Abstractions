b9ec8907603c7b717307659d3cc1936266438a39
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-10-31 23:13:35 -0400

    Write and implement IGuildMember

---
+++ DSharpPlus.Abstractions/Entities/Guild/Member/GuildMember.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using DSharpPlus.Net.Models;
+
+namespace DSharpPlus.Abstractions.Entities
+{
+	public sealed class GuildMember : IGuildMember
+	{
+		private readonly DiscordMember _member;
+		private readonly IDiscordClient _client;
+		
+		public GuildMember(DiscordMember member, IDiscordClient client)
+		{
+			_member = member;
+			_client = client;
+		}
+
+		public ulong Id => _member.Id;
+		public DateTimeOffset CreationTimestamp => _member.CreationTimestamp;
+		public string Username => _member.Username;
+		public DiscordColor? BannerColor => _member.BannerColor;
+		public string? BannerUrl => _member.BannerUrl;
+		public string AvatarUrl => _member.AvatarUrl;
+		public UserFlags? Flags => _member.Flags;
+		public string Mention => _member.Mention;
+		public bool IsCurrent => _member.IsCurrent;
+		public bool IsBot => _member.IsBot;
+		public string GetAvatarUrl(ImageFormat fmt, ushort size = 1024) => _member.GetAvatarUrl(fmt, size);
+		public string GuildAvatarUrl => _member.GuildAvatarUrl;
+		public string Nickname => _member.Nickname;
+		public string DisplayName => _member.DisplayName;
+		public DiscordColor Color => _member.Color;
+		public DateTimeOffset JoinedAt => _member.JoinedAt;
+		public IGuild Guild => _client.Guilds[_member.Guild.Id];
+		public bool IsOwner => _member.IsOwner;
+		public int Hierarchy => _member.Hierarchy;
+		public Permissions Permissions => _member.Permissions;
+		public async Task<IChannel> CreateDmChannelAsync() => throw new NotImplementedException();
+		public async Task<IMessage> SendMessageAsync(string content) => throw new NotImplementedException();
+		public async Task<IMessage> SendMessageAsync(DiscordEmbed embed) => throw new NotImplementedException();
+		public async Task<IMessage> SendMessageAsync(string content, DiscordEmbed embed) => throw new NotImplementedException();
+		public async Task<IMessage> SendMessageAsync(DiscordMessageBuilder builder) => throw new NotImplementedException();
+		public Task ModifyAsync(Action<MemberEditModel> action) => _member.ModifyAsync(action);
+		public Task BanAsync(int deleteDays = 0, string? reason = null) => _member.BanAsync(deleteDays, reason);
+		public Task UnbanAsync(string? reason = null) => _member.UnbanAsync(reason);
+		public Task PlaceInAsync(IChannel channel) => throw new NotImplementedException();
+		public Task UpdateVoiceStateAsync(IChannel channel, bool? suppress) => throw new NotImplementedException();
+		public Permissions PermissionsIn(IChannel channel) => _member.PermissionsIn(_member.Guild.Channels[channel.Id]);
+	}
+}
--- DSharpPlus.Abstractions/Entities/Guild/Member/IGuildMember.cs
+++ DSharpPlus.Abstractions/Entities/Guild/Member/IGuildMember.cs
@@ -1,7 +1,62 @@
-﻿namespace DSharpPlus.Abstractions.Entities
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using DSharpPlus.Net.Models;
+
+namespace DSharpPlus.Abstractions.Entities
 {
-	public interface IGuildMember : ISnowflake
+	public interface IGuildMember : IUser
 	{
+		public string GuildAvatarUrl { get; }
+
+		public string Nickname { get; }
+
+		public string DisplayName { get; }
 		
+		//TODO: Roles
+
+		public DiscordColor Color { get; }
+
+		public DateTimeOffset JoinedAt { get; }
+		
+		//TODO: Deafened/Muted
+		
+		public IGuild Guild { get; }
+
+		public bool IsOwner { get; }
+
+		public int Hierarchy { get; }
+
+		public Permissions Permissions { get; }
+
+		public Task<IChannel> CreateDmChannelAsync();
+
+		public Task<IMessage> SendMessageAsync(string content);
+
+		public Task<IMessage> SendMessageAsync(DiscordEmbed embed);
+
+		public Task<IMessage> SendMessageAsync(string content, DiscordEmbed embed);
+
+		public Task<IMessage> SendMessageAsync(DiscordMessageBuilder builder);
+		
+		//TODO: SetMute, SetDeaf
+
+		public Task ModifyAsync(Action<MemberEditModel> action);
+		
+		//TODO: GrantRoleAsync
+		//TODO: RevokeRoleAsync
+
+		public Task BanAsync(int deleteDays = 0, string? reason = null);
+
+		public Task UnbanAsync(string? reason = null);
+
+		public Task PlaceInAsync(IChannel channel);
+
+		public Task UpdateVoiceStateAsync(IChannel channel, bool? suppress);
+
+		public Permissions PermissionsIn(IChannel channel);
+		
+		
+
 	}
 }
